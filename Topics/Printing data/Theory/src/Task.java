16e441e0099cfc03580b6190dc85245758abb545
Aleksei <alex.usevich@gmail.com>
2021-06-04 14:23:02 +0300

    Initial commit

---
+++ Topics/Printing data/Theory/src/Task.java
@@ -0,0 +1,10 @@
+// You can experiment here, it won’t be checked
+
+public class Task {
+  public static void main(String[] args) {
+    System.out.print(108);   // printing a number
+    System.out.print('c');   // printing a character that represents a letter
+    System.out.print("Q");   // printing a string
+    System.out.println('3'); // printing a character that
+  }
+}
